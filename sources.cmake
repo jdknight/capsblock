267e5ee9100d41fc3c696e427ad72b1605819fc2
James Knight <james.d.knight@live.com>
2024-02-18 15:09:41 -0500

    build: include helper scripts in install target
    For a CMake install event, also ensure the helper scripts provided by
    the implementation are also included.
    
    Signed-off-by: James Knight <james.d.knight@live.com>

--- sources.cmake
+++ sources.cmake
@@ -4,3 +4,9 @@ set(CAPSBLOCK_SRCS
     ${SRC_DIR}/capsblock.rc
     ${SRC_DIR}/main.c
 )
+
+set(CAPSBLOCK_SCRIPTS
+    ${SCRIPTS_DIR}/install.bat
+    ${SCRIPTS_DIR}/shutdown.bat
+    ${SCRIPTS_DIR}/uninstall.bat
+)
